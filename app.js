27579c53c2d5c31c3ee046d4f591ee3cbe0aee31
Eric Beahan <ebeahan@gmail.com>
2018-07-02 13:49:28 -0500

    updating app

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan')
-var path = require('path')
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }
+];
 
-app.listen(3000, function(){
-    debug(`Listening on port ${chalk.green('3000')}`);
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
