b5d8b134afabb2650eeffefdea0cec927806f3db
Eric Beahan <ebeahan@gmail.com>
2018-07-17 18:07:43 -0500

    updating test init script

--- test/test-init.sh
+++ test/test-init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# Install Node.js
+apt-get install --yes curl
+curl --silent --location https://deb.nodesource.com/setup_10.x | sudo bash -
+apt-get install --yes nodejs
+apt-get install --yes build-essential
+
+# NPM deps
 npm install
+
+# Run tests
 mocha
